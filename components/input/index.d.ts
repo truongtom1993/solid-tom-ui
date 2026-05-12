53f4dde22e74be062c3e24fd460387e58168917e
MSI_TruongDN <truongtom1993@gmail.com>
2026-05-12 08:00:11 +0700

    v1.0.18

--- components/input/index.d.ts
+++ components/input/index.d.ts
@@ -1,3 +1,13 @@
 export { Input } from './input';
+export { InputText } from './variants/input-text';
+export { InputTextArea } from './variants/input-textarea';
+export { InputPassword } from './variants/input-password';
+export { InputNumber } from './variants/input-number';
+export { InputColor } from './variants/input-color';
+export { InputDate } from './variants/input-date';
+export { InputRange } from './variants/input-range';
+export { InputOTP } from './variants/input-otp';
+export { InputRadio } from './variants/input-radio';
 export type { InputBaseProps, InputTextProps, InputTextAreaProps, InputColorProps, InputDateProps, InputNumberProps, InputPasswordProps, InputRangeProps, InputOTPProps, } from './input.types';
+export type { InputRadioProps } from './variants/input-radio';
 //# sourceMappingURL=index.d.ts.map
